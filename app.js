69115e692f7a38ffa6ad50ad4fd5964cc483a967
JaviAlmada <javierealmada@gmail.com>
2021-03-04 16:11:27 -0300

    primeros pasos

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const {frutas, dinero} = require('./frutas')
+const express = require ('express');
+const app = express();
 
-frutas.forEach(item => {
-  console.count(item)
+const port = 5500;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  res.render("index", {titulo:'mi titulo dinamico'})
 })
 
-console.log(dinero);
+app.get('/servicios', (req, res) => {
+  res.render("servicios", {servicios:"servicios desde las rutas"})
+})
+
+app.listen(port, () => {
+  console.log('el servidor esta a su dispocicion en el puerto', port)
+})
+
+
+//app.use((req, res, next) => {
+  //res.status(404).sendFile(__dirname + '/public/404.html')
+//})
+
